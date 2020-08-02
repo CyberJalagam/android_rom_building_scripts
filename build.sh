844d05a3517199c750e506c95f33133eb043a71a
JAISHNAVPRASAD <62203009+CyberJalagam@users.noreply.github.com>
2020-08-02 22:55:29 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # screen -LS rom bash build.sh
 
 source build/envsetup.sh
-lunch lineage_CPH1859-userdebug
+lunch <build>_CPH1859-userdebug
 mka api-stubs-docs && mka hiddenapi-lists-docs && mka system-api-stubs-docs && mka test-api-stubs-docs && mka bacon
