9233d92a8f930de4a8494ec072c7339510e932e3
JAISHNAVPRASAD <62203009+CyberJalagam@users.noreply.github.com>
2020-08-25 15:03:23 +0530

    Script: Remove default codename

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 source build/envsetup.sh
-lunch <build>_CPH1859-userdebug
+lunch <build>_<codename>-userdebug
 mka api-stubs-docs && mka hiddenapi-lists-docs && mka system-api-stubs-docs && mka test-api-stubs-docs && mka bacon
