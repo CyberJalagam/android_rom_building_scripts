962884a61a3b5ee45eaa2f09dd81d4f0cf510503
JAISHNAVPRASAD <62203009+CyberJalagam@users.noreply.github.com>
2020-09-22 18:14:32 +0530

    Create personal_build.sh

---
+++ personal_build.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/CyberJalagam/scripts/master/gitsetup.sh
+bash gitsetup.sh
+wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh
+bash android_build_env.sh
+wget https://www.dropbox.com/s/w65lffvkkqvvj93/gdrive?dl=1
+mv gdrive?dl=1 gdrive
+chmod +x gdrive
+./gdrive list
