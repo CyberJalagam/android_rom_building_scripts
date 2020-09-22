a8be571d93feffc0ccd67f018a623bf143ae203a
JAISHNAVPRASAD <62203009+CyberJalagam@users.noreply.github.com>
2020-09-22 18:18:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 source build/envsetup.sh
 lunch <build>_<codename>-userdebug
-mka api-stubs-docs && mka hiddenapi-lists-docs && mka system-api-stubs-docs && mka test-api-stubs-docs && mka bacon
+mka bacon -j8
