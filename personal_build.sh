cbaf67654f5425c744db06e841c319ff2082fcb7
JAISHNAVPRASAD <62203009+CyberJalagam@users.noreply.github.com>
2020-10-08 13:19:22 +0530

    add ccache

--- personal_build.sh
+++ personal_build.sh
@@ -2,6 +2,10 @@ wget https://raw.githubusercontent.com/CyberJalagam/scripts/master/gitsetup.sh
 bash gitsetup.sh
 wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh
 bash android_build_env.sh
+export USE_CCACHE=1
+export USE_CCACHE_EXEC=$(command -v ccache)
+ccache -M 50G
+export ANDROID_JACK_VM_ARGS="-Xmx15g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
 wget https://www.dropbox.com/s/w65lffvkkqvvj93/gdrive?dl=1
 mv gdrive?dl=1 gdrive
 chmod +x gdrive
