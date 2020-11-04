7907b3165e3794c0b4e08896417ffc280c2bfa96
JAISHNAVPRASAD <62203009+CyberJalagam@users.noreply.github.com>
2020-11-04 12:29:35 +0530

    GITSETUP: made it easier for other users

--- gitsetup.sh
+++ gitsetup.sh
@@ -2,14 +2,23 @@
 #
 # Script to setup git
 
+GIT_USERNAME=""
+GIT_MAIL=""
+     
+echo "Enter GitHub Username: "
+read GIT_USERNAME
+
+echo "Enter GitHub Email: "
+read GIT_EMAIL
+
 # Username
 echo -e "\n================== Adding Username ==================\n"
-git config --global user.name "CyberJalagam"
+git config --global user.name "$GIT_USERNAME"
 echo -e "\n Added your username!\n"
 
 #Email
 echo -e "\n==================   Adding Email   ==================\n"
-git config --global user.email "rbinternationalnetwork@gmail.com"
+git config --global user.email "$GIT_MAIL"
 echo -e "\n Added your email!\n"
 
 #List Settings
