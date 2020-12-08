431b539d7c85a7aa8bf5ec328b657d4090f0f8e7
hitesh kamble <71580009+hitesh920@users.noreply.github.com>
2020-12-08 19:02:09 +0530

    Also install python 3.6

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh
 bash android_build_env.sh
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt-get -y update
+sudo apt-get install -y python3.6
 export USE_CCACHE=1
 export USE_CCACHE_EXEC=$(command -v ccache)
 ccache -M 50G
 export ANDROID_JACK_VM_ARGS="-Xmx15g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get -y update
-sudo apt-get install -y python3.6
