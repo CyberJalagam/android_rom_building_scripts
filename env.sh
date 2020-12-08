84128115121b4aacb4e7de623e2e38744bb4d614
hitesh kamble <71580009+hitesh920@users.noreply.github.com>
2020-12-08 18:48:33 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,3 +4,6 @@ export USE_CCACHE=1
 export USE_CCACHE_EXEC=$(command -v ccache)
 ccache -M 50G
 export ANDROID_JACK_VM_ARGS="-Xmx15g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt-get -y update
+sudo apt-get install -y python3.6
