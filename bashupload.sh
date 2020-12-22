4a12bbaad99884ede8605e9f9da9c3c7e817d957
hitesh kamble <71580009+hitesh920@users.noreply.github.com>
2020-12-22 16:29:59 +0530

    typo fix

--- bashupload.sh
+++ bashupload.sh
@@ -5,10 +5,10 @@ echo "<<<<< by hitesh920 >>>>>"
 FILE_NAME_WITH_EXTENSION=""
 FILE_LOCATION_WITH_EXTENSION=""
 
-echo "enter full rom name with extension"
+echo "Enter full rom name with extension"
 read FILE_NAME_WITH_EXTENSION
 
-echo "Enter full dir of rom zip including extension: "
+echo "Enter full dir of rom file including extension"
 read FILE_LOCATION_WITH_EXTENSION
 
 curl https://bashupload.com/"$FILE_NAME_WITH_EXTENSION" --data-binary @"$FILE_LOCATION_WITH_EXTENSION"
