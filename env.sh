4921e7b0251ff068d1e8f37ffec9b2c81ee63248
hitesh920 <hiteshkamble920@gmail.com>
2021-06-01 20:33:06 +0530

    UPLOAD: Switch to google drive
    - You can use "gdrive upload <filepath> or grive upload <filename>"

--- env.sh
+++ env.sh
@@ -20,6 +20,9 @@
 
 echo "<<<<< © RB INTERNATIONAL NETWORK™ >>>>>"
 
+chmod +x gdrive
+sudo install gdrive /usr/local/bin/gdrive
+gdrive list
 wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh
 bash android_build_env.sh
 sudo add-apt-repository -y ppa:deadsnakes/ppa
@@ -29,4 +32,5 @@ export USE_CCACHE=1
 export USE_CCACHE_EXEC=$(command -v ccache)
 ccache -M 50G
 export ANDROID_JACK_VM_ARGS="-Xmx15g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
+
 echo "<<<<< © RB INTERNATIONAL NETWORK™ >>>>>"
