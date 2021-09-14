7acc94f71dfc376ea05b40728693d737223720e2
JAISHNAVPRASAD <jaishnavprasad@disroot.org>
2021-09-14 20:14:56 +0530

    env: modify ccache commands

--- env.sh
+++ env.sh
@@ -41,9 +41,10 @@ sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get -y update
 sudo apt-get install -y python3.6
 export USE_CCACHE=1
-export USE_CCACHE_EXEC=$(command -v ccache)
-ccache -M 50G
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 100G
 export ANDROID_JACK_VM_ARGS="-Xmx15g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
+export CCACHE_COMPRESS=1
 
 echo -e "${CYAN}"
 echo "<<<<< © RB INTERNATIONAL NETWORK™ >>>>>"
